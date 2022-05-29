a63d1f56cb0145227fe4c75e0d9151562adb668a
lhl7 <75879657+lhl7@users.noreply.github.com>
2022-05-29 21:36:47 +0800

    优化了密码系统，通过读取配置文件判断

--- app/src/main/java/net/micode/notes/ui/MarkDownActivity.java
+++ app/src/main/java/net/micode/notes/ui/MarkDownActivity.java
@@ -2,7 +2,6 @@ package net.micode.notes.ui;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
